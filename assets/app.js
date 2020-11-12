475e37be8a90aa216a17b9309891d347d67b42a8
jobkeep3r <73900292+jobkeep3r@users.noreply.github.com>
2020-11-12 14:30:59 +0530

    search button added minor

--- assets/app.js
+++ assets/app.js
@@ -19,17 +19,17 @@ appReload = () => {
         chainId = await web3.eth.net.getNetworkType();
       }
       if (chainId == "main" && isEthereum) {
-        tokenContract = await JobKeep3rContact(tokenABI, tokenAdd);
-        presaleContract = await JobKeep3rContact(presaleABI, presaleAdd);
+        // tokenContract = await JobKeep3rContact(tokenABI, tokenAdd);
+        // presaleContract = await JobKeep3rContact(presaleABI, presaleAdd);
       } else if (chainId == "ropsten" && isEthereum) {
-        // msgPopupControl(
-        //   "show",
-        //   "Wrong Network",
-        //   "",
-        //   "Please Switch to Main Network",
-        //   "no",
-        //   `Close`
-        // );
+        msgPopupControl(
+          "show",
+          "Wrong Network",
+          "",
+          "Please Switch to Main Network",
+          "no",
+          `Close`
+        );
         tokenContract = await JobKeep3rContact(tokenABI_R, tokenAdd_R);
         presaleContract = await JobKeep3rContact(presaleABI_R, presaleAdd_R);
       } else {

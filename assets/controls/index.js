81c955b1208bc2deccaa72ef8302d0ab4f8550d0
jobkeep3r <73900292+jobkeep3r@users.noreply.github.com>
2020-11-25 16:24:39 +0530

    version update

--- assets/controls/index.js
+++ assets/controls/index.js
@@ -178,65 +178,65 @@ msgPopupControl = (visibility, headerMsg, bodyMsg, error, load, action) => {
 };
 buyPresaleToken = async () => {
   let Ether_Value = $("#presale_eth_value ").val();
-  // msgPopupControl(
-  //   "show",
-  //   "Transaction processing",
-  //   "Please confirm this transaction by your metamask wallet",
-  //   "none",
-  //   "load",
-  //   "Waiting for confirmation..."
-  // );
   msgPopupControl(
     "show",
-    "Work in Progress...",
-    "Work in Progress...",
+    "Transaction processing",
+    "Please confirm this transaction by your metamask wallet",
     "none",
-    "none",
-    "Close"
+    "load",
+    "Waiting for confirmation..."
   );
-  // if (isConnected) {
-  //   presaleContract.methods
-  //     .buyTokens_hdd()
-  //     .send({
-  //       from: currentAccount,
-  //       value: web3.utils.toWei(Ether_Value, "ether"),
-  //     })
-  //     .on("transactionHash", (hash) => {
-  //       // console.log("Hash" + hash);
-  //       msgPopupControl(
-  //         "show",
-  //         "Transaction processing",
-  //         "Please wait while we complete your transaction",
-  //         "none",
-  //         "load",
-  //         `<a  href="http://etherscan.io/tx/${hash}" target="_blank" rel="noopener noreferrer">View on EtherScan</a>`
-  //       );
-  //     })
-  //     .on("error", function (error) {
-  //       // console.log("error" + error.message);
-  //       msgPopupControl(
-  //         "show",
-  //         "Transaction processing",
-  //         "Transaction failed",
-  //         error.message,
-  //         "no",
-  //         `Close`
-  //       );
-  //     })
-  //     .on("confirmation", function (confirmationNumber, receipt) {
-  //       msgPopupControl(
-  //         "show",
-  //         "Transaction Completed",
-  //         `Congratulation, your transaction has be successfully Completed, ${
-  //           Ether_Value * presale_token_Rate
-  //         } ${tokenSymbol} has been successfully added in your wallet`,
-  //         "none",
-  //         "no",
-  //         `Close`
-  //       );
-  //       appReload();
-  //     });
-  // }
+  // msgPopupControl(
+  //   "show",
+  //   "Work in Progress...",
+  //   "Work in Progress...",
+  //   "none",
+  //   "none",
+  //   "Close"
+  // );
+  if (isConnected) {
+    presaleContract.methods
+      .buyTokens_hdd()
+      .send({
+        from: currentAccount,
+        value: web3.utils.toWei(Ether_Value, "ether"),
+      })
+      .on("transactionHash", (hash) => {
+        // console.log("Hash" + hash);
+        msgPopupControl(
+          "show",
+          "Transaction processing",
+          "Please wait while we complete your transaction",
+          "none",
+          "load",
+          `<a  href="http://etherscan.io/tx/${hash}" target="_blank" rel="noopener noreferrer">View on EtherScan</a>`
+        );
+      })
+      .on("error", function (error) {
+        // console.log("error" + error.message);
+        msgPopupControl(
+          "show",
+          "Transaction processing",
+          "Transaction failed",
+          error.message,
+          "no",
+          `Close`
+        );
+      })
+      .on("confirmation", function (confirmationNumber, receipt) {
+        msgPopupControl(
+          "show",
+          "Transaction Completed",
+          `Congratulation, your transaction has be successfully Completed, ${
+            Ether_Value * presale_token_Rate
+          } ${tokenSymbol} has been successfully added in your wallet`,
+          "none",
+          "no",
+          `Close`
+        );
+        appReload();
+      });
+  }
 };
 
 loadpresalevalues = () => {
@@ -370,22 +370,22 @@ addUnbond = async () => {
 };
 
 bondUnbondCall = () => {
-  msgPopupControl(
-    "show",
-    "Work in Progress...",
-    "Work in Progress...",
-    "none",
-    "none",
-    "Close"
-  );
+  // msgPopupControl(
+  //   "show",
+  //   "Work in Progress...",
+  //   "Work in Progress...",
+  //   "none",
+  //   "none",
+  //   "Close"
+  // );
 
-  // let bondtype = $(".bondtype").text();
+  let bondtype = $(".bondtype").text();
 
-  // if (bondtype == "Bond:") {
-  //   addBond();
-  // } else if (bondtype == "Unbond:") {
-  //   addUnbond();
-  // }
+  if (bondtype == "Bond:") {
+    addBond();
+  } else if (bondtype == "Unbond:") {
+    addUnbond();
+  }
 };
 
 activatePendingBond = async () => {
